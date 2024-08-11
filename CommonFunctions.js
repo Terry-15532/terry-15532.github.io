dd2a81171cffd1aa9f1f6fa36707b84a781e3646
Terry-15532 <kafuuchino_15532@foxmail.com>
2024-08-12 00:05:49 +0800

    更新

--- CommonFunctions.js
+++ CommonFunctions.js
@@ -7,9 +7,7 @@ function switchToPortfolio() {
 }
 
 function switchToAbout() {
-    // window.location.href = "index.html";
     SwitchPage("about.html", "About Me");
-    // ChangeHeader("About Me");
 }
 
 function setElm(id, name) {
@@ -31,13 +29,6 @@ var currHeader = "";
 function ChangeHeader(header) {
     var h = $(header);
     setElm("headerTitle", header);
-    // h.setAttribute("onClick", "");
-    // h.setAttribute("style", "color: white; transition: text-shadow 300ms; float: right; pointer-events: none;");
-    // data => data.text()).then(
-    // data => {
-    // h.setAttribute("style", "color: white; transition: text-shadow 300ms; float: right; pointer-events: none; text-shadow: 0px 0px 20px rgb(0, 233, 255);");
-    // }
-    // );
     dispLine(header + "_Line");
     h.style.pointerEvents = "none";
     h.style.textShadow = "0px 0px 20px rgb(0, 233, 255)";
@@ -47,36 +38,12 @@ function ChangeHeader(header) {
     $(tmp).style = "transition: text-shadow 300ms;";
 }
 
-// var currBody = 1;
-
 function SwitchPage(filename, header) {
     fetch(filename)
         .then(response => response.text())
         .then(data => {
-            // document.scrollTop = 0;
-            // document.documentElement.setAttribute("style", "transition: all 500ms; scrollTop: 0px;");
             document.documentElement.scrollTop = 0;
             setElm("Body", data);
-            // if (currBody == 2) {
-            //     currBody = 1;
-            //     // data.getElementById("Body_1").setAttribute("id", "Body_2");
-            //     $("Body_2").setAttribute("style", "position: absolute; margin-right: 100%; transition: all 500ms;");
-            //     $("Body_1").setAttribute("style", "position: absolute; margin-right: 0px; transition: all 500ms;");
-            // }
-            // else if (currBody == 1) {
-            //     currBody = 2;
-            //     // data.getElementById("Body_1").setAttribute("id", "Body_2");
-            //     setElm("Body_1", data);
-            //     $("Body_1").setAttribute("style", "position: absolute; margin-right: 100%; transition: all 500ms;");
-            //     $("Body_2").setAttribute("style", "position: absolute; margin-right: 0px; transition: all 500ms;");
-            // }
-            // if (currBody == 1) {
-            //     currBody = 2;
-            //     // data.getElementById("Body_2").setAttribute("id", "Body_1");
-            //     setElm("Body_1", data);
-            //     $("Body_1").setAttribute("style", "margin-right: 100%;");
-            //     $("Body_2").setAttribute("style", "margin:0px;");
-            // }
         })
     ChangeHeader(header);
 }
